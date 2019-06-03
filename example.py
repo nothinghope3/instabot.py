a4f5e52d13bad2c45a24fb8e5049ba79e8ad2eb6
nothinghope3 <32780914+nothinghope3@users.noreply.github.com>
2019-06-03 17:28:23 +0530

    Update example.py

--- example.py
+++ example.py
@@ -12,12 +12,12 @@ from src.unfollow_protocol import unfollow_protocol
 bot = InstaBot(
     login="apex.shroud",  # Enter username (lowercase). Do not enter email!
     password="gaurav9004933175",
-    like_per_day=1800,
+    like_per_day=2400,
     comments_per_day=240,
     tag_list=["apexlegends", "apexlegendspc", "apexlegendsps4", "apexlegendsxbox"],
     tag_blacklist=["", ""],
     user_blacklist={},
-    max_like_for_one_tag=450,
+    max_like_for_one_tag=600,
     follow_per_day=0,
     follow_time=1 * 60 * 60,
     unfollow_per_day=0,
